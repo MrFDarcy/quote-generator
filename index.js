5c1473a97e1ab01456e0bdd10c3d2cb432bfe086
Aditya Patil <patiladityadr@gmail.com>
2023-02-11 01:01:35 +0530

    Fetch API and updated the innerHTML

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+async function getData() {
+    const apiUrl = 'https://stoicquotesapi.com/v1/api/quotes/random';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        let quoteElement = document.getElementsByClassName('quote');
+        let authorElement = document.getElementsByClassName('author');
+
+        quoteElement[0].innerHTML = data.body;
+        authorElement[0].innerHTML = data.author;
+
+        console.log(data.body);
+        console.log(data.author);
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getData();
